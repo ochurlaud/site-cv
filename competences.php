b891d5389e3bb94cbe92c1f7fd619ce6f476338c
Olivier CHURLAUD <olivier@churlaud.com>
2014-07-05 15:39:02 +0200

    translate competences fr

--- competences.php
+++ competences.php
@@ -1,6 +1,14 @@
 <?php require_once('includes/headers.php');?>
-
-	<h1 class="page-header">Compétences</h1> 
+<?php
+$language = 'fr_FR.utf8';
+putenv("LANG=$language"); 
+setlocale(LC_ALL, $language);
+// Set the text domain as 'messages'
+$domain = 'messages';
+bindtextdomain($domain, "./locale"); 
+textdomain($domain);
+?>
+	<h1 class="page-header"><?php echo _("Compétences");?></h1> 
 
 	<div id="competences">
 		<div class="row">
@@ -8,11 +16,9 @@
 				<div class="thumbnail">
 					<h1 class="text-center"><b class="glyphicon glyphicon-road"></b></h1>
 					<div class="caption">
-						<h4>Management</h4>
+						<h4><?php echo _("comp_management_titre");?></h4>
 						<ul>
-							<li>Chef de projet</li>
-							<li>Coordination d'équipes</li>
-							<li>Gestion d'échéances</li>
+							<?php echo _("comp_management");?>
 						</ul>
 					</div>
 				</div>
@@ -21,11 +27,9 @@
 				<div class="thumbnail">
 					<h1 class="text-center"><b class="glyphicon glyphicon-random"></b></h1>
 					<div class="caption">
-						<h4>Sciences</h4>
+						<h4><?php echo _("comp_sciences_titre");?></h4>
 						<ul>
-							<li>Traitement du signal, Filtrage</li>
-							<li>&Eacute;lectromagnetisme</li>
-							<li>Mathématiques</li>
+							<?php echo _("comp_sciences");?>
 						</ul>
 					</div>
 				</div>
@@ -34,11 +38,9 @@
 				<div class="thumbnail">
 					<h1 class="text-center"><b class="glyphicon glyphicon-globe"></b></h1>
 					<div class="caption">
-						<h4>Langues</h4>
+						<h4><?php echo _("comp_langues_titre");?></h4>
 						<ul>
-							<li><strong>Francais</strong> : Langue maternelle</li>
-							<li><strong>Anglais</strong> : Lu, écrit, parlé (~fluide) - TOEFL 613pts (2014)</li>
-							<li><strong>Allemand</strong> : Lu, écrit, parlé (~fluide) - B2 (2014)</li>
+							<?php echo _("comp_langues");?>
 						</ul>
 					</div>
 				</div>
@@ -49,10 +51,9 @@
 				<div class="thumbnail">
 					<h1 class="text-center"><b class="glyphicon glyphicon-qrcode"></b></h1>
 					<div class="caption">
-						<h4>&Eacute;lectronique</h4>
+						<h4><?php echo _("comp_electronique_titre");?></h4>
 						<ul>
-							<li>Programmation de microcontrôleurs Microchip</li>
-							<li>Configuration modules Microchip (MRF24J40, MRF24WB0)</li>
+							<?php echo _("comp_electronique");?>
 						</ul>
 					</div>
 				</div>
@@ -62,12 +63,9 @@
 				<div class="thumbnail">
 					<h1 class="text-center"><b class="glyphicon glyphicon-hdd"></b></h1>
 					<div class="caption">
-						<h4>Informatique</h4>
+						<h4><?php echo _("comp_informatique_titre");?></h4>
 						<ul>
-							<li><strong>Administration système et réseaux Linux</strong></li>
-							<li>Virtualisation KVM</li>
-							<li>Bridging, VLAN</li>
-							<li>Apache2, Nginx</li>
+							<?php echo _("comp_informatique");?>
 						</ul>
 					</div>
 				</div>
@@ -76,11 +74,9 @@
 				<div class="thumbnail">
 					<h1 class="text-center"><b class="glyphicon glyphicon-comment"></b></h1>
 					<div class="caption">
-						<h4>Développement</h4>
+						<h4><?php echo _("comp_developpement_titre");?></h4>
 						<ul>
-							<li><strong>Web</strong> : HTML5, CSS, PHP, <em>Symfony2</em></li>
-							<li><strong>BDD</strong> : MySQL, PostgreSQL, MS Access</li>
-							<li><strong>Langages divers</strong> : MATLAB, C/C++, Latex</li>
+							<?php echo _("comp_developpement");?>
 						</ul>
 					</div>
 				</div>
--- cv.php
+++ cv.php
@@ -9,7 +9,7 @@ textdomain($domain);
 ?>
 	<div class="container-fluid col-lg-10 col-lg-offset-1">
 
-		<h1 class="page-header">&Eacute;tudes</h1> 
+		<h1 class="page-header"><?php echo _("cv_h1_etudes");?></h1> 
 
 		<div class="topic row">
 			<div class="topic-header col-sm-3 col-md-2">2012 - 2016</div>
@@ -30,8 +30,8 @@ textdomain($domain);
 			</div>
 		</div>
 		
-		<h1 class="page-header">Expériences</h1>
-			<h3>Direction de projets</h3>
+		<h1 class="page-header"><?php echo _("cv_h1_exp");?></h1>
+			<h3><?php echo _("cv_exp_titre");?></h3>
 		<div class="topic row">
 			<div class="topic-header col-sm-3 col-md-2">2013 - 2014</div>
 			<div class="topic-content col-sm-9 col-md-10">
@@ -63,7 +63,7 @@ textdomain($domain);
 			</div>
 		</div>
 		
-			<h3>Expériences professionnelles</h3>
+			<h3><?php echo _("cv_exp_pro_titre");?></h3>
 		<div class="topic row">
 			<div class="topic-header col-sm-3 col-md-2">2014</div>
 			<div class="topic-content col-sm-9 col-md-10">
@@ -77,33 +77,33 @@ textdomain($domain);
 			</div>
 		</div>
 		<div class="topic row">
-			<div class="topic-header col-sm-3 col-md-2">2010 et 2011</div>
+			<div class="topic-header col-sm-3 col-md-2">2010 <?php echo _("et"); ?> 2011</div>
 			<div class="topic-content col-sm-9 col-md-10">
 				<?php echo _("cv_exp_pro_demenagement"); ?>
 			</div>
 		</div>
 		
-		<h1 class="page-header">Compétences</h1>
+		<h1 class="page-header"><?php echo _("cv_h1_competences");?></h1>
 		<div class="topic row">
-			<div class="topic-header col-sm-3 col-md-2">Management</div>
+			<div class="topic-header col-sm-3 col-md-2"><?php echo _("cv_comp_management_header"); ?></div>
 			<div class="topic-content col-sm-9 col-md-10">
 				<?php echo _("cv_comp_management"); ?>
 			</div>
 		</div>
 		<div class="topic row">
-			<div class="topic-header col-sm-3 col-md-2">Sciences</div>
+			<div class="topic-header col-sm-3 col-md-2"><?php echo _("cv_comp_sciences_header"); ?></div>
 			<div class="topic-content col-sm-9 col-md-10">
 				<?php echo _("cv_comp_sciences"); ?>
 			</div>
 		</div>
 		<div class="topic row">
-			<div class="topic-header col-sm-3 col-md-2">Informatique</div>
+			<div class="topic-header col-sm-3 col-md-2"><?php echo _("cv_comp_informatique_header"); ?></div>
 			<div class="topic-content col-sm-9 col-md-10">
 				<?php echo _("cv_comp_informatique"); ?>
 			</div>
 		</div>
 		<div class="topic row">
-			<div class="topic-header col-sm-3 col-md-2">Langues</div>
+			<div class="topic-header col-sm-3 col-md-2"><?php echo _("cv_comp_langues_header"); ?></div>
 			<div class="topic-content col-sm-9 col-md-10">
 				<?php echo _("cv_comp_anglais"); ?><br />
 				<?php echo _("cv_comp_allemand"); ?>
@@ -111,29 +111,29 @@ textdomain($domain);
 		</div>
 		
 		<h1 class="page-header">Centres d’intérêt</h1>
-			<h3>Culturel</h3>
+			<h3><?php echo _("cv_interet_titre"); ?></h3>
 		<div class="topic row">
-			<div class="topic-header col-sm-3 col-md-2">Littérature</div>
+			<div class="topic-header col-sm-3 col-md-2"><?php echo _("cv_interet_litterature_header"); ?></div>
 			<div class="topic-content col-sm-9 col-md-10">
 				<?php echo _("cv_interet_litterature"); ?>
 			</div>
 		</div>
 		<div class="topic row">
-			<div class="topic-header col-sm-3 col-md-2">Cinéma</div>
+			<div class="topic-header col-sm-3 col-md-2"><?php echo _("cv_interet_cinema_header"); ?></div>
 			<div class="topic-content col-sm-9 col-md-10">
 				<?php echo _("cv_interet_cinema"); ?>
 			</div>
 		</div>
 		<div class="topic row">
-			<div class="topic-header col-sm-3 col-md-2">Musique</div>
+			<div class="topic-header col-sm-3 col-md-2"><?php echo _("cv_interet_musique_header"); ?></div>
 			<div class="topic-content col-sm-9 col-md-10">
 				<?php echo _("cv_interet_musique"); ?>
 			</div>
 		</div>
 
-		<h3>Sport</h3>
+		<h3><?php echo _("cv_interet_sport_titre"); ?></h3>
 		<div class="topic row">
-			<div class="topic-header col-sm-3 col-md-2">Sports de montagne</div>
+			<div class="topic-header col-sm-3 col-md-2"><?php echo _("cv_interet_sport_header"); ?></div>
 			<div class="topic-content col-sm-9 col-md-10">
 				<?php echo _("cv_interet_sport"); ?>
 			</div>
