0380b673a5572b011383eca488586983802fa986
Olivier CHURLAUD <olivier@churlaud.com>
2014-07-24 00:08:11 +0200

    add php file that handles language processing

---
+++ includes/languages.php
@@ -0,0 +1,35 @@
+<?php
+	if (!isset($_SESSION['lang']))
+	{
+		$_SESSION['lang'] = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+	}
+
+	if (isset($_POST['lang']))
+	{
+		if ($_POST['lang'] == 'fr')
+			$_SESSION['lang'] = 'fr';
+		elseif ($_POST['lang'] == 'en')
+			$_SESSION['lang'] = 'en';
+	}
+
+	switch ($_SESSION['lang'])
+	{
+		case 'fr':
+			$language = "fr_FR.utf8";
+			$btn_fr = 'btn-info';
+			$btn_en = '';
+			break;
+		default:
+			$language = "en_US.utf8";
+			$btn_fr = '';
+			$btn_en = 'btn-info';
+			break;
+	}		
+
+	putenv("LANG=$language"); 
+	setlocale(LC_ALL, $language);
+	// Set the text domain as 'messages'
+	$domain = 'messages';
+	bindtextdomain($domain, "./locale"); 
+	textdomain($domain);
+?>
