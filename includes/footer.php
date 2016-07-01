5bc71b606e33c5cecbe24b152511a10a3115bc26
Olivier CHURLAUD <olivier@churlaud.com>
2016-07-01 18:14:50 +0200

    revert git (randomly supported by OVH)

--- includes/footer.php
+++ includes/footer.php
@@ -1,17 +1,20 @@
 </div>
 
 <?php
-    $git_date = `git log -n 1 --format=%ct`;
-    $hash = `git log -n 1 --format=%h`;
-    $descript = `git log -n 1 --format=%s`;
-    $date = strftime('%d %b %Y', intval($git_date));
+    $log_file = escapeshellarg('.git/logs/HEAD');
+    $last_commit = `tail -n 1 $log_file`; // Read the last line of the file
+    $last_commit_expl = explode(' ', $last_commit, 8);
+    $last_commit_date = $last_commit_expl[5];
+    $last_commit_hash = substr($last_commit_expl[1], 0, 7);  // Keep only the 7 first digits of the hash
+    $last_commit_descript = substr($last_commit_expl[7], 0, -1); // Remove the space at the end of the line
 ?>
     <div id="footer">
         <div class="container center-block">
-            Olivier CHURLAUD &ndash; <?php echo $date; ?>
-            <span style="color:#555;   font-style:italic;"> (<?php echo $hash .': ' . $descript . ')';?></span>
+            Olivier CHURLAUD &ndash; <?php echo strftime('%d %B %Y',$last_commit_date); ?>
+            <span style="color:#555;   font-style:italic;"> (<?php echo $last_commit_hash .': ' . $last_commit_descript . ')';?></span>
         </div>
     </div>
+
 
     <script>
         var page = location.pathname.substring(location.pathname.lastIndexOf("/") + 1)
