51048fd4d900079bd3e332c3e7a362e9e264b196
Olivier CHURLAUD <olivier@churlaud.com>
2016-08-15 20:25:56 +0200

    Add online presence

--- includes/bandeau.php
+++ includes/bandeau.php
@@ -9,15 +9,15 @@
                 <strong>Olivier CHURLAUD</strong><br />
                 <?php echo $age . ' ' . _("ans") . ' (' ._("né le 7 avril 1992") .')';?><br />
                 <span class="glyphicon glyphicon-send"></span> <span class="adressemail">moc.dualruhc@reivilo</span><br />
-                <img src="img/linkedin_nb.png" width="14px" /> <a href="<?php echo _("linkedin");?>" alt="lien linkedin"><?php echo _("linkedin");?></a><br />
-                <span class="glyphicon glyphicon-earphone"></span> +49 (0)1575-2931348<br />
+                <span class="glyphicon glyphicon-earphone"></span> +49 (0)1575-2931348 <br />
             </address>
         </div>
         <div class="col-sm-6 col-md-4">
             <address>
-                <span class="glyphicon glyphicon-envelope"></span> 12, place du grand four<br />
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;79 370 MOUGON</br />
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FRANCE
+                <strong><?php echo _("presence_online"); ?></strong><br />
+                <img src="img/linkedin_nb.png" width="14px" /> <a href="<?php echo _("linkedin");?>" alt="lien linkedin">LinkedIn</a><br />
+                <img src="img/github.png" width="14px" /> <a href="https://github.com/ochurlaud" alt="lien github">GitHub</a><br />
+                <span class="glyphicon glyphicon-console"></span> <a href="irc://freenode/ochurlaud">ochurlaud</a> <?php echo _("sur"); ?> Freenode
             </address>
         </div>
     </div>
